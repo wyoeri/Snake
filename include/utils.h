3c26a9dd854e405bedc8adf8d06acbac552c9019
wyoeri <nnagornmaksim@gmail.com>
2026-04-01 17:32:28 +0300

    feat: add common utility functions and color macros

---
+++ include/utils.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdbool.h>
+
+// text colors 
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+
+// reset changes
+#define RESET_COLOR "\033[0m"
+#define RESET_TEXT "\033[0m"
+
+// text formatting
+#define BOLD_TEXT "\033[1m"
+
+// error messages
+void msg_input_error(void);
+
+// messages before entering values
+void msg_selection(void);
+
+// checking numbers
+bool check_number_i(int *x);
+
+// other
+void clear_screen(void);
+
+#endif
---
+++ src/utils.c
@@ -0,0 +1,27 @@
+#include "utils.h"
+
+#include <stdio.h>
+
+// error messages
+void msg_input_error(void){
+    printf("%sPlease enter a valid value!%s\n", RED, RESET_COLOR);
+} 
+
+// messages before entering values
+void msg_selection(void){
+    printf("Select an option: ");
+}
+
+// checking numbers
+bool check_number_i(int *x){
+    if(scanf("%d", x) != 1){
+        while(getchar() != '\n');
+        return false;
+    }
+    return true;
+}
+
+// other
+void clear_screen(void){
+    printf("\033[2J\033[H");
+}
